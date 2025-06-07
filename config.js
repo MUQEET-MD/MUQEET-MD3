663302c441c9809cf7befa166a6088c94a1ca310
MUQEET-MD <quotexmarketview0@gmail.com>
2025-06-07 18:58:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "STARK-ALI~XtIlTCaI#TucZOTXFwtz90Vf_LPaAagcz4XJwXxRFUr9EYLOpUMw",
+SESSION_ID: process.env.SESSION_ID || "WZhFQJ6S#tM7RW9UePnY11nUe8igxrT0y_uhwbPzNet2zGykV4LA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
